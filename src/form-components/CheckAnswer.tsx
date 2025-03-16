9c336792d940cd78adb9f42b8cd038a9fc4cd839
Peter <petecht@udel.edu>
2025-03-16 15:03:58 -0400

    check answer

--- src/form-components/CheckAnswer.tsx
+++ src/form-components/CheckAnswer.tsx
@@ -5,9 +5,24 @@ export function CheckAnswer({
 }: {
     expectedAnswer: string;
 }): React.JSX.Element {
+    const [currentAnswer, setCurrentAnswer] = useState<string>("");
+
+    function changeAnswer(event: React.ChangeEvent<HTMLInputElement>) {
+        setCurrentAnswer(event.target.value);
+    }
+
+    let correct: boolean = currentAnswer === expectedAnswer;
+
     return (
         <div>
             <h3>Check Answer</h3>
+            <input
+                type="text"
+                value={currentAnswer}
+                onChange={changeAnswer}
+                placeholder="Answer"
+            />
+            <span style={{ marginLeft: "10px" }}>{correct ? "✔️" : "❌"}</span>
         </div>
     );
 }
